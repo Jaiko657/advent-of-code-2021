ffecede96e58f1d0bd933c3bf7193b41c7a7880f
Jaiko657 <matthewboyd03@gmail.com>
2021-12-02 15:30:37 +0000

    Day 2: Dive! Part 1

---
+++ day2/Program.cs
@@ -0,0 +1,22 @@
+﻿string[] instructions = File.ReadAllLines("./input");
+int depth = 0;
+int forward = 0;
+foreach(string line in instructions)
+{
+    int amount = Convert.ToInt32(Char.GetNumericValue(line, line.Length - 1));
+    switch(line[0])
+    {
+        case 'f':
+            forward = forward + amount;
+            break;
+        case 'u':
+            depth = depth - amount;
+            break;
+        case 'd':
+            depth = depth + amount;
+            break;
+    }
+}
+Console.WriteLine("Depth: " + depth.ToString());
+Console.WriteLine("Forward: " + forward.ToString());
+Console.WriteLine("Product: " + (depth*forward).ToString());
