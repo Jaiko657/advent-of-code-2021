fde41bb78402f0f4387e516875b96d14574d4e86
Jaiko657 <matthewboyd03@gmail.com>
2021-12-02 14:32:55 +0000

    Day 1: Sonar Sweep. Part 1

---
+++ day1/Program.cs
@@ -0,0 +1,28 @@
+﻿// int[] depths = new int[] { 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 };
+string[] strdepths = File.ReadAllLines("./input");
+int[] depths = Array.ConvertAll<string, int>(strdepths, int.Parse);
+int count = 0;
+for (int i = 0; i < depths.Length; i++)
+{
+    Console.Write(depths[i].ToString());
+    if(i == 0)
+    {
+        Console.Write(" (N/A - no previous measurement)");
+    } else 
+    {
+        if(depths[i] > depths[i - 1])
+        {
+            count++;
+            Console.Write(" (increased)");
+        } else
+        {
+            if(depths[i] < depths[i - 1])
+            {
+                Console.Write(" (decreased)");
+            }
+        }
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.Write("Amount of Depth Increases: " + count.ToString());
