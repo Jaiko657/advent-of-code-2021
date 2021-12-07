29f476ff241b88e0aaab8b29c05c5a44d8cc5edf
Jaiko657 <matthewboyd03@gmail.com>
2021-12-07 18:32:56 +0000

    Day 6: Lanternfish. Part 1

---
+++ day6/Program.cs
@@ -0,0 +1,31 @@
+﻿string input = File.ReadLines("./input").First();
+int[] State = input.Split(',').Select(int.Parse).ToArray();
+
+static int[] startNextDay(int[] fishSchool)
+{
+    int fishToBeBorn = 0;
+    for (int i = 0; i < fishSchool.Length; i++)
+    {
+        if (fishSchool[i] == 0) fishToBeBorn++;
+        fishSchool[i] = fishSchool[i] != 0 ? fishSchool[i]-1 : 6;
+    }
+    fishSchool = addFish(fishSchool, fishToBeBorn);
+    return fishSchool;
+}
+
+static int[] addFish(int[] fishSchool, int amountofFish)
+{
+    for (int i = 0; i < amountofFish; i++)
+    {
+        fishSchool = fishSchool.Append(8).ToArray();
+    }
+    return fishSchool;
+}
+
+for (int i = 0; i < 80; i++)
+{
+    State = startNextDay(State);
+    Console.WriteLine(i);
+}
+
+Console.WriteLine(State.Length.ToString());
