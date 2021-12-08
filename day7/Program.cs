5a9149a2c9c5655a0264c62fa04fbe2eddc353bf
Jaiko657 <matthewboyd03@gmail.com>
2021-12-08 18:11:08 +0000

    Day 7: Submarine Crabs (incomplete)

---
+++ day7/Program.cs
@@ -0,0 +1,35 @@
+﻿string input = File.ReadLines("./input").First();
+int[] State = input.Split(',').Select(int.Parse).ToArray();
+
+static int calculateFuel(int positionAlligned, int[] State)
+{
+    int fuelUsed = 0;
+    for (int i = 0; i < State.Length; i++)
+    {
+        int crabPosition = State[i];
+        fuelUsed += Math.Abs(crabPosition - positionAlligned);
+    }
+    return fuelUsed;
+}
+
+static int bruteForceBestPosition(int[] State)
+{
+    int position = -69;
+    int fuel = 1000000000;
+    //create array with number in array
+    int max = State.Max();
+    for(int i  = 0; i < max; i++)
+    {
+        int newfuel = calculateFuel(i, State);
+        if (newfuel < fuel)
+        {
+            fuel = newfuel;
+            position = i;
+        }
+    }
+    return position;
+}
+
+
+// Console.WriteLine(State.Max());
+Console.WriteLine(bruteForceBestPosition(State));
