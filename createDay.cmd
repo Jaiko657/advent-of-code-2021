a68169424fdc24e027b28d4d23a849412cda1bbe
Jaiko657 <matthewboyd03@gmail.com>
2021-12-09 12:56:15 +0000

    Changed createDay to include input Parsing

--- createDay.cmd
+++ createDay.cmd
@@ -3,5 +3,7 @@ SET name=day%1
 mkdir %name%
 cd %name%
 dotnet new console
+SET input=string[] input = File.ReadAllLines("./input");
+echo %input%> Program.cs
 tasklist /FI "IMAGENAME eq VSCodium.exe" 2>NUL | find /I /N "VSCodium.exe">NUL
 if "%ERRORLEVEL%"=="1" codium.cmd ..
