e4e808aa80e721fbf270088a7af8a3be82726f52
Jaiko657 <matthewboyd03@gmail.com>
2021-12-10 20:57:37 +0000

    Day 10: Syntax Scoring. Part 2(incomplete)

--- day10/Program.cs
+++ day10/Program.cs
@@ -1,9 +1,29 @@
 string[] input = File.ReadAllLines("./input");
-var answer = 0;
+List<string> validLines = new List<string>();
 foreach (string line in input)
 {
-    answer += findFirstIllegalPair(line);
+    if(checkIfLineValid(line))
+    {
+        validLines.Add(removePairs(line));
+    }
 }
+List<int> scores = new List<int>();
+foreach(string valid in validLines)
+{
+    int score = 0;
+    Console.WriteLine(valid);
+    for (int i = valid.Length-1; i > -1 ; i--)
+    {
+        char letter = valid[i];
+        if (letter == '(') score = (score*5)+1;
+        if(letter == '[') score = (score*5)+2;
+        if(letter == '{') score = (score*5)+3;
+        if(letter == '<') score = (score*5)+4;
+    }
+    scores.Add(score);
+    Console.WriteLine(score);
+}
+Console.WriteLine(GetMedian(scores.ToArray()));
 static string removePairs(string input)
 {
     while(true)
@@ -28,13 +48,32 @@ static string removeTrailingBrackets(string input)
         }
     }
 }
-static int findFirstIllegalPair(string input)
+static bool checkIfLineValid(string input)
 {
     input = removePairs(input);
-    if (input.Contains("[)")||input.Contains("{)")||input.Contains("<)")) return 3;
-    if (input.Contains("[}")||input.Contains("(}")||input.Contains("<}")) return 1197;
-    if (input.Contains("(]")||input.Contains("{]")||input.Contains("<]")) return 57;
-    if (input.Contains("[>")||input.Contains("{>")||input.Contains("(>")) return 25137;
-    return 0;
+    if (input.Contains("[)")||input.Contains("{)")||input.Contains("<)")) return false;
+    if (input.Contains("[}")||input.Contains("(}")||input.Contains("<}")) return false;
+    if (input.Contains("(]")||input.Contains("{]")||input.Contains("<]")) return false;
+    if (input.Contains("[>")||input.Contains("{>")||input.Contains("(>")) return false;
+    return true;
 }
-Console.WriteLine(answer);
+static int GetMedian(int[] sourceNumbers) {
+        //Framework 2.0 version of this method. there is an easier way in F4        
+        if (sourceNumbers == null || sourceNumbers.Length == 0)
+            throw new System.Exception("Median of empty array not defined.");
+
+        //make sure the list is sorted, but use a new array
+        int[] sortedPNumbers = (int[])sourceNumbers.Clone();
+        Array.Sort(sortedPNumbers);
+        foreach (int i in sortedPNumbers)
+        {
+            Console.Write(i + ", ");
+        }
+        //get the median
+        int size = sortedPNumbers.Length;
+        int mid = size / 2;
+        int median = (size % 2 != 0) ? (int)sortedPNumbers[mid] : ((int)sortedPNumbers[mid] + (int)sortedPNumbers[mid - 1]) / 2;
+        return median;
+    }
+// 1701163 to low
+// 1483081 to low
